c87c645581b1e4b589008922487c5949fb9e8c77
rtx40060 <moto.sebstian@gmail.com>
2025-05-20 19:47:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,13 @@
 function adam(){
     document.getElementById("dwa").textContent = "";
     const sredniauz = parseInt(document.getElementById("srednia").value);
-    if(sredniauz >= 2){
+    if(sredniauz < 2){
+        document.getElementById("dwa").textContent = `NieEeEeEeEEe, zostaw zeszyt na drugi rok`
+    }else if (sredniauz<=6 && sredniauz >=2){
         document.getElementById("dwa").textContent = `Gratulacje zdajesz! gud dżob`
     }else{
-        document.getElementById("dwa").textContent = `NieEeEeEeEEe, zostaw zeszyt na drugi rok`
+        document.getElementById("dwa").textContent = `I co próbujesz i tak nie zdasz`
     }
+
+
 }
